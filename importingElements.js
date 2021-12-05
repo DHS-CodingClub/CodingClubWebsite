0fc2f45d9e971b9387a5046e3a471af261af815f
Arden Zarins <55220658+azarins1@users.noreply.github.com>
2021-12-04 16:11:22 -0800

    Initial commit, have a basic framework set out

---
+++ importingElements.js
@@ -0,0 +1,13 @@
+const imports = document.getElementById("imports");
+imports.innerHTML =
+    `<link rel="stylesheet" href="style.css">
+    <div id="navbar">
+        <img src="logo.png" height=40 style="display: inline-block;">
+        <h1 style="display: inline;">DHS Coding Club</h1>
+        <br>
+        <a href="">The Team</a>
+        <a href="">Events</a>
+        <a href="">Projects</a>
+        <a href="">Coding Tournament</a>
+        <a href="">Projects</a>
+    </div>`;
